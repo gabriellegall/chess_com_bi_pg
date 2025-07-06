db9e3f59c8b6401789f265f73614641fedae216a
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-07-06 21:29:14 +0200

    adedd clustering

--- models/intermediate/int_games_moves.sql
+++ models/intermediate/int_games_moves.sql
@@ -2,7 +2,8 @@
     materialized = 'incremental',
     unique_key = ['uuid','move_number'],
     post_hook=[
-        "CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_uuid ON {{ this }} (uuid)"
+        "CREATE INDEX IF NOT EXISTS idx_{{ this.name }}_uuid ON {{ this }} (uuid)",
+        "CLUSTER {{ this.render() }} USING idx_{{ this.name }}_uuid"
     ]
 ) }}
 

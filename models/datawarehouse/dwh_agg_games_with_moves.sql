e7774bf79ba4df57704506d094af9e565071f836
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-07-18 22:52:47 +0200

    renamed view

--- models/datawarehouse/dwh_agg_games_with_moves.sql
+++ models/datawarehouse/dwh_agg_games_with_moves.sql
@@ -1,5 +1,5 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'view'
 ) }}
 
 SELECT 
@@ -7,4 +7,4 @@ SELECT
     (COUNT(*) OVER (
         PARTITION BY username, time_class, end_time_month, opponent_rating_range, game_phase_key
     ) > {{ var('datamart')['min_games_played'] }}) AS has_enough_games
-FROM {{ ref('dwh_agg_games_with_moves__tmp') }}
+FROM {{ ref('dwh_agg_games_with_moves__prep') }}

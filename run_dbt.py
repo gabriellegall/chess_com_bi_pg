e4626027efebe4bd81d2c9a8a6eb13c9ab6db055
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-07-20 17:13:15 +0200

    added opener

--- run_dbt.py
+++ run_dbt.py
@@ -8,7 +8,7 @@ import os
 def run_pipeline():
 
     load_dotenv()
-    subprocess.run(["dbt", "run",], check=True)
+    subprocess.run(["dbt", "run", "--full-refresh",], check=True)
 
 if __name__ == "__main__":
     run_pipeline()

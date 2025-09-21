aef0b3431c48dce734a0a9889195470280519c87
Gabriel LE GALL <gabrielgall@hotmail.fr>
2025-09-21 11:03:04 +0200

    added files

--- streamlit/data/agg_games_with_moves__games.sql
+++ streamlit/data/agg_games_with_moves__games.sql
@@ -4,9 +4,9 @@ FROM dwh.dwh_agg_games_with_moves
 WHERE TRUE
     AND aggregation_level       = 'Games'
     AND playing_rating_range    = opponent_rating_range
-    -- AND playing_rating_range    = '0800-1000'
+    AND playing_rating_range    = '0800-1000'
     AND playing_result IN ('Win', 'Lose')
     AND username = 'Zundorn'
-    -- AND end_time >= '2025-05-01'
-    AND playing_as = 'White'
+    AND end_time >= '2025-05-01'
+    AND playing_as = 'Black'
     -- AND time_control = '600'

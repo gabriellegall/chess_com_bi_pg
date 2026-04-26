4e7a7018c4a603d35f9507b5b6513baed67e71b8
Gabriel LE GALL <71171030+gabriellegall@users.noreply.github.com>
2026-04-26 19:50:25 +0200

    added placeholder for definition

--- dbt/run_all.py
+++ dbt/run_all.py
@@ -66,7 +66,7 @@ def run_pipeline_forever():
                     requests.get(URL_DBT_TEST + "/fail", timeout=5)
 
             # Sleep
-            time.sleep(10)
+            time.sleep(600) # Sleep for 10 minutes
 
         except Exception as e:
             requests.get(URL + "/fail", timeout=5)
